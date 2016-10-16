7eed7990cc027085b8126da91420ba914d2e5b04
Roberto Beccari <info@robertobeccari.it>
2016-10-16 22:32:51 +0200

    Add project files.

---
+++ TDD.Example.ONE.Test/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDD.Example.ONE.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void sm_CheckWithoutContent()
+        {
+            var sm = new StringManager();
+
+            Assert.IsTrue(sm.IsNullOrEmpty);
+        }
+
+        [TestMethod]
+        public void sm_CheckStringAdd()
+        {
+            var sm = new StringManager();
+
+            var _s = "Roberto";
+
+            Assert.AreEqual(sm.Add(_s).ToString(), "roberto");
+        }
+    }
+}
---
+++ TDD.Example.ONE/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD.Example.ONE
+{
+    public class StringManager
+    {
+        private string StringValue { get; set; }
+
+        public StringManager()
+        {
+            StringValue = string.Empty;
+        }
+
+        public StringManager Add(string s)
+        {
+            StringValue += s;
+            return this;
+        }
+
+        public StringManager Remove(string s)
+        {
+            StringValue.Replace(s, string.Empty);
+            return this;
+        }
+
+        public bool IsNullOrEmpty
+        {
+            get {
+                return string.IsNullOrEmpty(this.StringValue);
+            }
+        }
+
+        public override string ToString()
+        {
+            return this.StringValue ?? string.Empty;
+        }
+    }
+}
